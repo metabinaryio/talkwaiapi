ac36c20f9be04f2d856ccf83c97d1721226b1020
Fazil Akbulut <fazilakbulut@outlook.com>
2020-02-05 04:32:53 +0300

    Update index.php

--- example/index.php
+++ example/index.php
@@ -1,8 +1,7 @@
 <?php
 include __DIR__ . "/vendor/autoload.php";
-
 $talkwai = new \itsdevel\talkwaiapi\Talkwai();
 
 $talkwai->setCredentials('CLIENT_KEY', 'DEVELOPER_SECRET');
 
-echo $talkwai->query("Hello!");
+$talkwai->query("Hello!");
